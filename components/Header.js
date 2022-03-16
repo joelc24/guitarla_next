b9debe93d11254015e923dfcedc2bca266f5dd91
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-03-16 17:00:02 -0500

    finalizando el css del header

--- components/Header.js
+++ components/Header.js
@@ -5,18 +5,20 @@ const Header = () => {
     return ( 
         <header className={styles.header}>
             <div className="contenedor">
+                <div className={styles.barra}>
                 <div>
                     <Link href="/">
                         <Image width={400} height={100} src="/img/logo.svg" alt="Imagen logo" />
                     </Link>
                 </div>
 
-                <nav>
+                <nav className={styles.navegacion}>
                     <Link href="/">Inicio</Link>
                     <Link href="/nosotros">Nosotros</Link>
                     <Link href="/blog">Blog</Link>
                     <Link href="/tienda">Tienda</Link>
                 </nav>
+                </div>
             </div>
         </header>
      );
