ac698d4a7a4e6c0c9cdd158c50616ea78a621f7c
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-03-22 11:52:29 -0500

    Creando la pagina de nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,26 @@
+import Image from 'next/image'
 import Layout from '../components/Layout';
+import styles from '../styles/Nosotros.module.css'
 const Nosotros = () => {
     return ( 
         <div>
             <Layout
                 pagina='Nosotros'
             >
-                <h1>Desde Nosotros</h1>
+                <main className='contenedor'>
+                    <h2 className='heading'>Nosotros</h2>
+
+                    <div className={styles.contenido}>
+
+                        <Image layout="responsive" width={600} height={450} src='/img/nosotros.jpg' alt="Imagen sobre nosotros"/>
+
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus ut felis facilisis accumsan ac ac felis. Nulla in aliquam magna. Morbi pretium bibendum lectus, non interdum metus pretium eu. Curabitur semper luctus tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean ultricies neque ut lectus tristique, vel elementum eros convallis. Nam sagittis, quam at faucibus molestie, metus lacus posuere lectus, non congue nulla lacus at nunc. Aliquam pellentesque sed magna sed tempor. Aliquam tellus elit, viverra sit amet felis luctus, aliquam fringilla velit. </p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus ut felis facilisis accumsan ac ac felis. Nulla in aliquam magna. Morbi pretium bibendum lectus, non interdum metus pretium eu. Curabitur semper luctus tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean ultricies neque ut lectus tristique, vel elementum eros convallis. Nam sagittis, quam at faucibus molestie, metus lacus posuere lectus, non congue nulla lacus at nunc. Aliquam pellentesque sed magna sed tempor. Aliquam tellus elit, viverra sit amet felis luctus, aliquam fringilla velit. </p>
+                        </div>
+                    </div>
+                </main>
 
             </Layout>
 
