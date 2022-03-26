f0ad2587b1c463afa51145779338417e0a3df1ff
Joel <libra-marriaga@hotmail.com>
2022-03-26 18:10:14 -0500

    Consultando la api con getServersideProps

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,9 @@
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({entradas}) => {
+
+    console.log(entradas);
+
     return ( 
         <Layout
             pagina='Blog'
@@ -8,6 +11,21 @@ const Blog = () => {
             <h1>Desde Blog</h1>
         </Layout>
      );
+}
+
+export async function getStaticProps(){
+    const url = 'http://localhost:1337/blogs/'
+
+    const respuesta = await fetch(url)
+    const entradas = await respuesta.json()
+
+    console.log(entradas)
+
+    return {
+        props: {
+            entradas
+        }
+    }
 }
  
 export default Blog;
