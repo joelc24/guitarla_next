28028641c4d97d2978bef965acd66aa8dc039080
Joel <libra-marriaga@hotmail.com>
2022-03-29 16:15:25 -0500

    css al bloque de cursos

--- components/Curso.js
+++ components/Curso.js
@@ -1,3 +1,5 @@
+import styles from '../styles/Curso.module.css'
+
 const Curso = ({curso}) => {
     const {titulo, contenido, imagen} = curso
     return ( 
@@ -10,6 +12,18 @@ const Curso = ({curso}) => {
                     <a href="#">Mas Informacion</a>
                 </div>
             </div>
+
+            <style jsx>{`
+                section {
+                    padding: 10rem 0;
+                    margin-top: 10rem;
+                    background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb(0 0 0 / .65)), url(${imagen.url});
+                    background-size: cover;
+                    background-position: center;
+                }
+            `}
+            </style>
+
         </section>
     );
 }
