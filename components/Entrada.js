a7feb0a680ecc69a8c0beca809cf6406a7e78f1b
Joel <libra-marriaga@hotmail.com>
2022-03-28 19:32:10 -0500

    Mostrando las imagenes

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,9 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { formatearFecha } from '../helpers'
 const Entrada = ({entrada}) => {
     const {titulo, resumen, imagen, published_at, id} = entrada
     return ( 
         <article>
+
+            <Image
+                src={imagen.url}
+                alt={`Imagen blog ${titulo}`}
+                width={800}
+                height={600}
+                layout="responsive"
+            />
+
             <div>
                 <h1>{titulo}</h1>
                 <p>{formatearFecha(published_at)}</p>
