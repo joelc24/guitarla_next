35316a1a8f6724a4feb6da55f364f3261404ce06
Joel <libra-marriaga@hotmail.com>
2022-03-29 12:53:31 -0500

    Consultar la API para obtener las guitarras

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,13 +1,26 @@
 import Layout from "../components/Layout";
 
-const Tienda = () => {
+const Tienda = ({guitarras}) => {
+    console.log(guitarras)
     return ( 
         <Layout
             pagina='Tienda Virtual'
         >
-            <h1>Desde Tienda Virtual</h1>
+            <h1 className="heading">Nuestra Coleccion</h1>
         </Layout>
      );
+}
+
+export async function getServerSideProps(){
+    const url = `${process.env.API_URL}/guitarras`
+    const respuesta = await fetch(url)
+    const guitarras = await respuesta.json()
+
+    return {
+        props: {
+            guitarras
+        }
+    }
 }
  
 export default Tienda;
