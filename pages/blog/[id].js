55585fe7b09e39d29ba28ba2acd3a0f7b9b507f1
Joel <libra-marriaga@hotmail.com>
2022-03-29 10:01:24 -0500

    Consultando la api para tener informacion de la entrada del blog

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,7 +5,22 @@ const EntradaVlog = ({entrada}) => {
     );
 }
 
-export async function getServerSideProps({query: {id}}) {
+export async function getStaticPaths(){
+    const url = 'http://localhost:1337/blogs/'
+    const respuesta = await fetch(url)
+    const entradas = await respuesta.json()
+    const paths = entradas.map(entrada => ({
+        params: { 
+            id: entrada.id.toString()
+        }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params: {id}}) {
     
     const url = `http://localhost:1337/blogs/${id}`
     const respuesta = await fetch(url)
@@ -16,5 +31,17 @@ export async function getServerSideProps({query: {id}}) {
         }
     }
 }
+
+/* export async function getServerSideProps({query: {id}}) {
+    
+    const url = `http://localhost:1337/blogs/${id}`
+    const respuesta = await fetch(url)
+    const entrada = await respuesta.json()
+    return {
+        props: {
+            entrada
+        }
+    }
+} */
  
 export default EntradaVlog;
