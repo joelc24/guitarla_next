1f4a3740e73335b1b9a50673fe2dc4b3abd790ef
Joel <libra-marriaga@hotmail.com>
2022-03-29 11:53:59 -0500

    Creando una pagina 404

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import styles from '../styles/NoEncontrado.module.css'
+const NoEncontrado = () => {
+    return ( 
+        
+        <div className={styles.no_encontrado}>
+            <h1 className="heading">Pagina no entrada</h1>
+
+            <Link href="/">Volver al Inicio</Link>
+        </div>
+        
+        
+    );
+}
+ 
+export default NoEncontrado;
