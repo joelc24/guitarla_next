cc5b438c4b36779070c47b14ab1367518bc11a4b
Joel <libra-marriaga@hotmail.com>
2022-03-29 09:16:00 -0500

    Routing dinamico en next.js

---
+++ pages/blog/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+const EntradaVlog = () => {
+    const router = useRouter()
+    return (    
+        <h1>Entrada Blog</h1>
+    );
+}
+ 
+export default EntradaVlog;
