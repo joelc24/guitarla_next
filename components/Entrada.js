320fd805bef444b33c04e0f8858e12dfd93fa9f2
Joel <libra-marriaga@hotmail.com>
2022-03-28 19:24:26 -0500

    Mostrando el resto de informacion

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,8 +1,17 @@
+import Link from 'next/link'
+import { formatearFecha } from '../helpers'
 const Entrada = ({entrada}) => {
     const {titulo, resumen, imagen, published_at, id} = entrada
     return ( 
         <article>
-            <h2>{titulo}</h2>
+            <div>
+                <h1>{titulo}</h1>
+                <p>{formatearFecha(published_at)}</p>
+                <p>{resumen}</p>
+                <Link href={`/blog/${id}`}>
+                    Leer Entrada
+                </Link>
+            </div>
         </article>
      );
 }
